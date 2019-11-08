ad1da9f9d2ec369f0d49580844e74a8878dae8f6
Miguel Boanovam <mgl.deadly@gmail.com>
2019-11-08 17:54:46 -0300

    alterado inputs

--- src/components/ModalCreateBook.js
+++ src/components/ModalCreateBook.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Modal, Form, Input, Icon } from 'antd';
+import { Modal, Form, Input, Icon, Select } from 'antd';
 
 function ModalCreateBook({ active, setActive, form: { getFieldDecorator, validateFields } }) {
     const [loading, setLoading] = useState(false)
@@ -28,45 +28,50 @@ function ModalCreateBook({ active, setActive, form: { getFieldDecorator, validat
         >
             <Form>
                 <Form.Item>
-                    {getFieldDecorator('firstname', {
-                        rules: [{ required: true, message: 'Digite seu primeiro nome' }],
+                    {getFieldDecorator('title', {
+                        rules: [{ required: true, message: 'Digite o titulo do livro' }],
                     })(
                         <Input
                             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="Nome"
+                            placeholder="Titulo"
                         />,
                     )}
                 </Form.Item>
                 <Form.Item>
-                    {getFieldDecorator('lastname', {
-                        rules: [{ required: true, message: 'Digite seu sobrenome!' }],
+                    {getFieldDecorator('ISBN', {
+                        rules: [{ required: true, message: 'Digite o ISBN do livro!' }],
                     })(
                         <Input
                             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="Sobrenome"
+                            placeholder="ISBN"
+                            type="number"
                         />,
                     )}
                 </Form.Item>
                 <Form.Item>
-                    {getFieldDecorator('email', {
-                        rules: [{ required: true, message: 'Digite seu email!' }],
+                    {getFieldDecorator('publicationDate', {
+                        rules: [{ required: true, message: 'Digite a data de publicação!' }],
                     })(
                         <Input
-                            prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="E-mail"
-
+                            prefix={<Icon type="calendar" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Data de publicação"
+                            type="date"
                         />,
                     )}
                 </Form.Item>
                 <Form.Item>
-                    {getFieldDecorator('password', {
-                        rules: [{ required: true, message: 'Digite sua senha!' }],
-                    })(
-                        <Input
-                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            type="password"
-                            placeholder="Senha"
-                        />,
+                    {getFieldDecorator('genre', {})(
+                        <Select
+                            placeholder="Selecione um genero"
+                        >
+                            <Select.Option value="COMEDY"> Comédia </Select.Option>
+                            <Select.Option value="DRAMA">Drama </Select.Option>
+                            <Select.Option value="FANTASY">Fantasia </Select.Option>
+                            <Select.Option value="ACTION">Ação </Select.Option>
+                            <Select.Option value="ADVENTURE"> Aventura </Select.Option>
+                            <Select.Option value="HORROR"> Horror </Select.Option>
+                            <Select.Option value="ROMANCE"> Romance </Select.Option>
+                        </Select>,
                     )}
                 </Form.Item>
             </Form>
